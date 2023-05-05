9930a6c6b0cfabab9695611b2a606f8cabac65ce
theloshackers <theloshackers@gmail.com>
2023-05-05 21:01:18 +1000

    update

---
+++ im5-xcode/im5-xcode/im5_xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  im5_xcodeApp.swift
+//  im5-xcode
+//
+//  Created by admin on 4/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct im5_xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
